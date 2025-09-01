d3d4f0de79a61bbc997fb3f8fe87dc863b805f5b
cooperx33 <cooperx33@gmail.com>
2025-08-31 19:46:42 -0500

    subnet

--- 3-subnet.tf
+++ 3-subnet.tf
@@ -1,24 +1,22 @@
 # https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/compute_subnetwork
-resource "google_compute_subnetwork" "winsub" {
-  name                     = "iowacorn"
-  ip_cidr_range            = "10.13.13.0/24"
+resource "google_compute_subnetwork" "armat3-subnet" {
+  name                     = "armat3-subnet"
+  ip_cidr_range            = "10.100.13.0/24"
   region                   = "us-central1"
   network                  = google_compute_network.armat3-vpc.id
  private_ip_google_access = true
 }
 
-resource "google_compute_subnetwork" "linsub" {
-  name                     = "osaka1"
-  ip_cidr_range            = "10.13.23.0/24"
-  region                   = "asia-northeast2"
+
+
+
+resource "google_compute_subnetwork" "regional_proxy_sub" {
+  name                     = "regional-proxy-sub"
+  ip_cidr_range            = "10.100.23.0/24"
+ # purpose = "Regional_managed_proxy"
+  region                   = "us-central1"
+  purpose                  = "REGIONAL_MANAGED_PROXY"
   network                  = google_compute_network.armat3-vpc.id
-  private_ip_google_access = true
+  role                     = "ACTIVE"
 }
 
-resource "google_compute_subnetwork" "linsub2" {
-  name                     = "madrid"
-  ip_cidr_range            = "10.13.33.0/24"
-  region                   = "europe-southwest1"
-  network                  = google_compute_network.armat3-vpc.id
-  private_ip_google_access = true
-}
